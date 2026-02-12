3b7d7d3491cfd366fc83aa8fef01e3efad50c066
Mujtaba Hussain <hussainmujtaba223@gmail.com>
2026-02-12 19:33:21 +0530

    Add character stats and joke printing to First.py
    This is my first Python program and I used pyjokes library here as well.

---
+++ First.py
@@ -0,0 +1,14 @@
+print('''Chopper 1000
+      Brook 350
+      Nami 360
+      Franky 380
+      Robin 980
+      Sanji 1093
+      Jinbie 1100
+      Zoro 1110
+      Luffy 3000''')
+# this program print a command
+import pyjokes
+joke=pyjokes.get_joke()
+print(joke)
+print("was a joke")
