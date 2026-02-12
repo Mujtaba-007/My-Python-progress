3cb84f7e73960c24b17ab8af85668429feea6dd9
Mujtaba Hussain <hussainmujtaba223@gmail.com>
2026-02-12 21:09:00 +0530

    Add files via upload

---
+++ file_write.py
@@ -0,0 +1,13 @@
+m="Mujtaba is the Second in command"
+z="Red is the Wild card"
+a="Green is the leader"
+f1=open("Bro1.txt","w")#Writing data in file1
+f2=open("Bro2.txt","w")#Writing data in file2
+f3=open("Bro3.txt","w")#Writing data in file3
+f1.write(a)
+f2.write(z)
+f3.write(m)
+f1.close
+f2.close
+f3.close
+#All files must be closed
---
+++ with_in_file.py
@@ -0,0 +1,3 @@
+with open("Bro1.txt") as f:
+    print(f.read())
+#no need to close the file ny "f.close()"
