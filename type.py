934e4d0b8df95abaccb47cea76f9e4a142533369
Mujtaba Hussain <hussainmujtaba223@gmail.com>
2026-02-12 19:31:22 +0530

    Basic printing program in type.py

---
+++ type.py
@@ -0,0 +1,11 @@
+# Type function and type conversion
+'''a=99.9
+f=type(a)
+print(f)'''
+#input function
+a= int(input("Enter number:- "))
+b= int(input("Enter number:- "))
+print("number a is: ",a)
+print("number b is: ",b)
+sum=a+b
+print("Sum= ",sum)
